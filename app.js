44aef164d7851b646e6e190c05d1552d35f3ef01
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-09-08 00:17:03 +0530

    updated java script

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+$(".menuButton").click(function() {
+    $(".homeScreen").hide();
+    $(".menuScreen").show();
+});
+
 $(".message").click(function() {
     $(".homeScreen").hide();
     $(".chatScreen").show();
+});
+
+$(".").click(function() {
+    $(".").hide();
+    $(".").show();
+});
+
+$(".").click(function() {
+    $(".").hide();
+    $(".").show();
+});
+
+$(".").click(function() {
+    $(".").hide();
+    $(".").show();
+});
+
+$(".").click(function() {
+    $(".").hide();
+    $(".").show();
 });
